10059324110ff24f416b60d5841e06a1d61590a6
dengyingjun <dyjun2030@sina.com>
2017-06-19 21:44:17 +0800

    第三次提交

--- src/org/git/demo/Demo.java
+++ src/org/git/demo/Demo.java
@@ -5,6 +5,7 @@ public class Demo {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!!");
 		System.out.println("Hello 张三!!");
+		System.out.println("Hello 王五!!");
 	}
 
 }
